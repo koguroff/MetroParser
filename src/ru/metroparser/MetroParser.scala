6ba9cf2946723077672dd1762dd0ae4dea6b78c3
koguro <koguroff@gmail.com>
2014-01-26 19:19:48 +0400

    First commit

---
+++ src/ru/metroparser/MetroParser.scala
@@ -0,0 +1,12 @@
+package ru.metroparser
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Koguro
+ * Date: 03.12.13
+ * Time: 1:02
+ * To change this template use File | Settings | File Templates.
+ */
+object MetroParser extends App{
+    println("Hello, world!")
+}
